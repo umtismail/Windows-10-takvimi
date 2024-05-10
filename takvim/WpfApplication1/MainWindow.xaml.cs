3eb6d399252adf317ed3e1dee8510d15f886e9e2
umtismail <157217252+umtismail@users.noreply.github.com>
2024-05-10 17:16:50 +0300

    takvim

---
+++ takvim/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.IsEnabled = true;
+            timer.Tick += timersay;
+            DateTime dt = DateTime.Now;
+            Date.Content = dt.ToLongDateString();
+        }
+
+        private void timersay(object sender, EventArgs e)
+        {
+            DateTime simdi = DateTime.Now;
+            time.Content = simdi.ToLongTimeString();
+        }
+
+        private void cl1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            day.Content = turkce(cl1.SelectedDate.Value.DayOfWeek.ToString()) + "  " + cl1.SelectedDate.Value.Day.ToString();
+            if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Saturday" || cl1.SelectedDate.Value.DayOfWeek.ToString() == "Sunday")
+            {
+                MessageBox.Show("Randevu alınamaz");
+            }
+        
+        }
+        private string turkce(string tr)
+        { 
+            string gun="" ;
+
+            if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Monday")
+            {
+                tr = "Pazartesi";
+            }
+
+            else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Tuesday")
+            {
+                tr = "Salı";
+            }
+
+            else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Wednesday")
+            {
+                tr = "Çarşamba";
+            }
+
+            else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Thursday")
+            {
+                tr = "Perşembe";
+            }
+
+            else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Friday")
+            {
+                tr = "Cuma";
+            }
+
+          else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Saturday")
+            {
+                tr = "Cumartesi";
+            }
+
+            else if (cl1.SelectedDate.Value.DayOfWeek.ToString() == "Sunday")
+            {
+                tr = "Pazar";
+            }
+            else
+            {
+                gun = tr;
+            }
+            return tr;
+        }
+    
+    }
+}
